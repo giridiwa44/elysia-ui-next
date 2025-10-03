aac06900ac9faf76093d716b8bf940b85a4e16b9
Giri Diwa Adam <giridiwa44@gmail.com>
2025-10-03 18:09:37 +0700

    perubahan

--- app/pages/docs/getting-started.vue
+++ app/pages/docs/getting-started.vue
@@ -1,10 +1,16 @@
 <template>
   <h1>{{ pageTitle }}</h1>
   <p>
-    Terimakasih banyak telah mencoba komponen Alphine gratis kami! Kami sedang
-    berupaya memperluas pustaka Alphine.js dan Tailwind CSS kami dengan lebih
-    banyak contoh di masa mendatang, jadi nantikan terus informasi terbarunya.
+    Thank you so much for trying out our free Alphine components! We're working
+    on expanding our Alphine.js and Tailwind CSS libraries with more examples in
+    the future, so stay tuned for more updates.
   </p>
+  <p>Untuk Menggunakan UI Kami Ada beberapa Persyaratan yang harus Anda Penuhi Di antaranya :</p>
+  <ul>
+  <li><a href="https://alphine.dev" class="font-bold">Alphine JS 3.x</a></li>
+  <li><a href="https://alphine.dev/plugins/focus" class="font-bold">Focus Plugins Alphine</a></li>
+  </ul>
+
 </template>
 <script setup>
 import { computed } from "vue";
